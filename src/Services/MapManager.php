488a2e531cf497454b90edd4de6a77a71d906ce6
Angelus-Dev-GitHub <t.dufosse666@gmail.com>
2021-01-13 20:12:42 +0100

    add login logout register

--- src/Services/MapManager.php
+++ src/Services/MapManager.php
@@ -5,7 +5,6 @@ namespace App\Services;
 
 
 use App\Entity\Player;
-use App\Entity\Tile;
 use App\Repository\PlayerRepository;
 use App\Repository\TileRepository;
 use phpDocumentor\Reflection\Types\True_;
@@ -31,24 +30,5 @@ class MapManager
     }
 
 
-    public function getRandomIsland(TileRepository $tileRepository)
-    {
-        $tiles = $tileRepository->findBy(['type' => 'island']);
-        $treasureIsland = $tiles[array_rand($tiles, $num = 1)];
-
-        return $treasureIsland;
-    }
-
-    public function checkTreasure(Player $player, TileRepository $tileRepository)
-    {
-        $result = false;
-        $treasureIsland = $tileRepository->findBy(['hasTreasure' => true]);
-        if (($treasureIsland[0]->getCoordX() == $player->getCoordX()) and ($treasureIsland[0]->getCoordY() == $player->getCoordY()) ){
-            return $result = true;
-        }
-
-        //dd($result);
-        return $result;
-    }
 
 }
