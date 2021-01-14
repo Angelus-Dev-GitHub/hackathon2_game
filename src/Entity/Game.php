3caecbccd888c25f09d00e8a6df79b081b7f6d85
Angelus-Dev-GitHub <t.dufosse666@gmail.com>
2021-01-14 16:50:31 +0100

    resolve select one avatar

---
+++ src/Entity/Game.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GameRepository::class)
+ */
+class Game
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $nPlayer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNPlayer(): ?int
+    {
+        return $this->nPlayer;
+    }
+
+    public function setNPlayer(?int $nPlayer): self
+    {
+        $this->nPlayer = $nPlayer;
+
+        return $this;
+    }
+}
